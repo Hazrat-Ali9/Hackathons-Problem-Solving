34e46a6f8429c5afa6190fd2e5e4adf4e4b4c42c
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-05-27 22:17:22 +0600

    Alice and Bob buy crakers 🚃

--- Alice and Bob buy crackers.cpp
+++ Alice and Bob buy crackers.cpp
@@ -3,24 +3,25 @@ using namespace std;
 
 int main()
 {
-	  ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-	  int m;
-      cin >> m;
-	  int cost[m];
-	  for(int i = 0; i < m; i++)
+
+
+    int m;
+    cin >> m;
+    int cost[m];
+    for(int i = 0; i < m; i++)
         cin >> cost[i];
-	  int firecrakers = 0;
-	  bool temp[20000 * m] = {0};
-	  for(int i = 0; i < (1<<m); i++)
+    int firecrakers = 0;
+    bool temp[20000 * m] = {0};
+    for(int i = 0; i < (1<<m); i++)
     {
-		    long long int sum = 0;
-		    for(int j = 0; j < m; j++)
+        long long int sum = 0;
+        for(int j = 0; j < m; j++)
             if(i & (1<<j))
                 sum += cost[j];
-		    if(!(temp[sum]) && sum && !(sum & 1))
+        if(!(temp[sum]) && sum && !(sum & 1))
             firecrakers++;
-		    temp[sum] = {1};
-	  }
-	  cout << firecrakers;
-	  return 0;
+        temp[sum] = {1};
+    }
+    cout << firecrakers;
+    return 0;
 }
