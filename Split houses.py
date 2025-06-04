c7f21d4ede2613ecf7b6d30e5fb1a670010ebd9b
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-06-04 09:31:27 +0600

    Spilt Houses 😫

---
+++ Split houses.py
@@ -0,0 +1,28 @@
+def temp():
+    n = int(input())
+    s = input()
+    house = []
+    count = 0
+    arr = [''] * n
+    
+    for i, j in enumerate(s):
+        if j == 'H':
+            arr[i] = 'H'
+            count += 1
+            if i == n - 1:
+                house.append(count)
+        else:
+            arr[i] = 'B'
+            if count > 0:
+                house.append(count)
+                count = 0
+
+    if len(house) == 0 or max(house) == 1:
+        print('YES')
+        print(''.join(arr))
+    else:
+        print('NO')
+
+# Call the function
+temp()
+
